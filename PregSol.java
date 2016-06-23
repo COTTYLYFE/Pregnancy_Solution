cd81fb962e951f04f5d10e00cd1b459956bc1710
YOUR NAME <YOUR@EMAIL.com>
2016-06-23 02:18:06 -0400

    First commit

---
+++ PregSol.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class PregSol {
+
+	public static void main (String args[]){
+		new PregSol();
+	}
+	
+	public PregSol(){
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+								//let n be the int of 
+								//siblings (with each set
+								//of twins being counted
+								//as one
+								
+		int k = in.nextInt();
+								//let k be the int of 
+								//years in the parents'
+								//possible sex time 
+								//(usually marriage
+								//years)
+								
+		int avgF = 2*52*(k-3/4*n);
+								//average times sex
+								//occured assuming
+								//2 fucks per week
+								
+		int posChi = avgF*99.
+		
+		System.out.println("");
+		
+		//TODO: Finish project
+		//TODO: Add anal percentage (given by user)
+		
+	}
+
+}
